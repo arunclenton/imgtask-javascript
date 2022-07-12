39312e351ab237a1035a2593c54165f526fbaaa8
arunclenton <arunclenten@gmail.com>
2022-07-12 21:33:52 +0530

    add

--- img.js
+++ img.js
@@ -1,26 +1,29 @@
 var increase = document.getElementsByClassName('in')[0];
 
-// 
-
 increase.addEventListener('click',function(){
 
-// c
-var img = document.querySelector('img');
-// console.log(img)
-var currentwidth = img.width;
-console.log(img.width)
 
- img.style.width =(img.width + 30) +'px';
+var img = document.querySelector('img');
+var currentwidth = img.width;
+
+if(currentwidth < 300){
+console.log(currentwidth)
+
+ img.style.width =(currentwidth + 30) +'px';
+}
  })
 
  var decrease =document.getElementsByClassName('out')[0];
-
+ 
  decrease.addEventListener('click',function(){
-
-// console.log('arun')
+  
     var img = document.querySelector('img');
-    // console.log(img)
+
+    var currentwidth = img.width;
+    if(currentwidth > 50){
+      console.log(currentwidth)
 
     img.style.width =(img.width - 30) +'px';
+    }
  })
  
